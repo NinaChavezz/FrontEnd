ab7e3299a2b4c021eeac1da47e72e4ab101551c6
NinaChavezz <chavez.carolinaok@gmail.com>
2023-05-10 17:06:59 -0300

    ediacercade

--- src/app/components/acerca-de/edit-acercade.component.ts
+++ src/app/components/acerca-de/edit-acercade.component.ts
@@ -10,7 +10,7 @@ import { PersonaService } from 'src/app/service/persona.service';
   styleUrls: ['./edit-acercade.component.css']
 })
 export class EditAcercadeComponent implements OnInit {
-  persona: persona;
+  persona: persona = new persona ("","","","");
   
   constructor(private activatedRouter: ActivatedRoute, private personaService: PersonaService, private router: Router, public imageService: ImageService) {}
 
@@ -28,7 +28,6 @@ export class EditAcercadeComponent implements OnInit {
 
   onUpdate(): void {
     const id = this.activatedRouter.snapshot.params['id'];
-    this.persona.img = this.imageService.url
     this.personaService.update(id, this.persona).subscribe(
       data => {
         this.router.navigate(['']);
@@ -37,13 +36,6 @@ export class EditAcercadeComponent implements OnInit {
         this.router.navigate(['']);
       }
     )
-
-  }
-
-  uploadImage($event:any){
-    const id = this.activatedRouter.snapshot.params['id'];
-    const name = "perfil_" + id;
-    this.imageService.uploadImage($event, name)
 
   }
 
--- src/app/model/persona.model.ts
+++ src/app/model/persona.model.ts
@@ -2,13 +2,14 @@ export class persona{
     id?: number;
     nombre: string;
     apellido: string;
-    descripcion: string
+    descripcion: string;
     img: string;
+    
 
     constructor(nombre: string, apellido: string, descripcion: string, img: string){
         this.nombre = nombre;
         this.apellido = apellido;
         this.descripcion = descripcion;
-        this.img = img;
+        this.img = img
     }
 }
